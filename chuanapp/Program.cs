f91061eaba6b9e376bae4efe9eb537bba9ce801e
leechuanhung <163072144+leechuanhung@users.noreply.github.com>
2024-03-12 11:17:55 +0900

    GiftBox 추가
    Letter와 Money를 넣고 배송기사님도 넣어뒀다.

--- chuanapp/Program.cs
+++ chuanapp/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace chuanapp
 {
+    class GiftBox
+    {
+        public string Letter;
+        public int Money;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GiftBox a = new GiftBox()
+            {
+                Letter = "잘 지내니?",
+                Money = 1000000
+            };
+            GiftBox 배송기사님 = a;
+
+            a.Letter = "어떻게 지내고 있어?";
+            a.Money = 1500000;
         }
     }
 }
