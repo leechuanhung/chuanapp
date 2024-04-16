5ae61aa1faa1b8d9d818b972b09cbfeaca55d1a1
leechuanhung <163072144+leechuanhung@users.noreply.github.com>
2024-04-16 09:31:39 +0900

    value type

--- chuanapp/Program.cs
+++ chuanapp/Program.cs
@@ -1,14 +1,29 @@
 ﻿namespace chuanapp
 {
-   
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("시작 부분");
 
-            GiftBoxManager manager = new GiftBoxManager();
-            manager.SetInit();
+            int aMoney = 10000;
+            int bMoney = aMoney;
+            aMoney = 20000;
+            Console.WriteLine("aMoney : " + aMoney);
+            Console.WriteLine("bMoney : " + bMoney);
+
+            GiftBox aGiftBox = new GiftBox()
+            {
+                Letter = "안녕",
+                Money = 10000
+            };
+
+            GiftBox bGiftBox = aGiftBox;
+            aGiftBox.Money = 20000;
+            Console.WriteLine("aGiftBox : " + aGiftBox.Money);
+            Console.WriteLine("bGiftBox : " + bGiftBox.Money);
 
         }
 
